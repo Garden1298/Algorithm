fd143a5eb04be486fe6ccfe6619205c452b9fc03
홍정원 <54656129+Garden1298@users.noreply.github.com>
2023-04-09 22:36:58 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N; 
+
+	cin >> N;
+
+    if (N % 7 == 1 || N % 7 == 3) 
+    {
+        cout << "CY";
+    }
+    else 
+    {
+        cout << "SK";
+    }
+}
