2cb58abb1ce1fb36afa24bedb36d021ca4c0b1fb
홍정원 <54656129+Garden1298@users.noreply.github.com>
2023-04-16 09:10:52 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	long long N; 
+
+	cin >> N;
+
+    if (N % 2) 
+    {
+        cout << "SK";
+    }
+    else 
+    {
+        cout << "CY";
+    }
+}
