ef04e8920b8e342d3187932272bcb4cceb4a79bb
홍정원 <54656129+Garden1298@users.noreply.github.com>
2023-05-27 22:48:33 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅂ, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23808. 골뱅이 찍기 － ㅂ/골뱅이 찍기 － ㅂ.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void EmptyBox(int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cout << "@";
+		}
+
+		for (int j = 0; j < N * 3; j++)
+		{
+			cout << " ";
+		}
+
+		for (int j = 0; j < N; j++)
+		{
+			cout << "@";
+		}
+		cout << endl;
+	}
+}
+
+void FullBox(int N)
+{
+	for (int j = 0; j < N; j++)
+	{
+		for (int i = 0; i < N * 5; i++)
+		{
+			cout << "@";
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	EmptyBox(N);
+
+	EmptyBox(N);
+
+	FullBox(N);
+
+	EmptyBox(N);
+
+	FullBox(N);
+}
