0756980c9b458f4d735ff32c211227c9e784e9dc
홍정원 <54656129+Garden1298@users.noreply.github.com>
2023-06-11 23:42:43 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+
+	while (true)
+	{
+		getline(cin, str);
+		if (str == "END")
+		{
+			break;
+		}
+		reverse(str.begin(), str.end());
+		cout << str << endl;
+	}
+}
