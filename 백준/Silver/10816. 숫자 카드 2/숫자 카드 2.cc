bdd7094ba42403e39820213caca5acd2482c2d5a
홍정원 <54656129+Garden1298@users.noreply.github.com>
2023-07-21 23:51:04 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 304 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,11 +1,13 @@
 #include <iostream>
-#include <map>
+//#include <map>
+#include<algorithm>
 
 using namespace std;
 
 int N, M;
 int num;
-map<int, int> m;
+//map<int, int> m;
+int arr[500002];
 
 int main()
 {
@@ -14,18 +16,34 @@ int main()
 
 	cin >> N;
 
-	// 입력받은 숫자의 값을 하나 더하기
+	//for (int i = 0; i < N; i++)
+	//{
+	//	cin >> num;
+	//	m[num]++;
+	//}
+
 	for (int i = 0; i < N; i++)
 	{
 		cin >> num;
-		m[num]++;
+
+		arr[i] = num;
 	}
 
+	sort(arr, arr + N);
+
+	//cin >> M;
+
 	cin >> M;
+
+	//for (int i = 0; i < M; i++)
+	//{
+	//	cin >> num;
+	//	cout << m[num] << " ";
+	//}
 
 	for (int i = 0; i < M; i++)
 	{
 		cin >> num;
-		cout << m[num] << " ";
+		cout << upper_bound(arr, arr + N, num) - lower_bound(arr, arr + N, num) << " ";
 	}
 }
