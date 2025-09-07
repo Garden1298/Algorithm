7279a18edaf4fc283d0aaaa8990291a731274f89
홍정원 <54656129+Garden1298@users.noreply.github.com>
2025-09-07 16:41:29 +0900

    [Gold V] Title: 리벤지 오브 피타고라스, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4563. 리벤지 오브 피타고라스/리벤지 오브 피타고라스.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    long long A;
+    while (true) {
+        cin >> A;
+        if (A == 0) break;
+
+        long long A2 = (long long)pow(A, 2);
+        int cnt = 0;
+
+        for (long long i = 1; i <= A; i++) {
+            if (A2 % i == 0) {
+                long long sub = i;
+                long long sum = A2 / i;
+
+                if ((sum - sub) / 2 > A && (sub - sum) % 2 == 0) {
+                    cnt++;
+                }
+            }
+        }
+
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
