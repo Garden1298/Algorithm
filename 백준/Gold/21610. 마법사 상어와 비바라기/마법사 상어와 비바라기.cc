bbfe6eb1c083319c4974ace320567159e5c102c6
홍정원 <54656129+Garden1298@users.noreply.github.com>
2025-11-30 23:19:52 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 4 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.cc
@@ -1,114 +1,76 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
-int map[51][51];
 int N, M;
-
-int dir = 1;
+int A[51][51];
 int dx[9] = { 0, 0,-1,-1,-1,0,1,1,1 };
-int dy[9] = { 0, -1,-1,0,1,1,1,0,-1 };
+int dy[9] = { 0,-1,-1,0,1,1,1,0,-1 };
 
 queue<pair<int, int>> cloud;
 
-int main()
-{
-	cin >> N >> M;
+int main() {
+    cin >> N >> M;
 
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			cin >> map[i][j];
-		}
-	}
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+            cin >> A[i][j];
 
-	//처음 구름 위치 추가
-	cloud.push(make_pair(N, 1));
-	cloud.push(make_pair(N, 2));
-	cloud.push(make_pair(N - 1, 1));
-	cloud.push(make_pair(N - 1, 2));
+    cloud.push({N, 1});
+    cloud.push({N, 2});
+    cloud.push({N - 1, 1});
+    cloud.push({N - 1, 2});
 
-	int d, s;
-	for (int i = 0; i < M; i++)
-	{
-		cin >> d >> s;
+    while (M--) {
+        int d, s;
+        cin >> d >> s;
 
-		int size = cloud.size();
-		bool isVisited[51][51] = { false, };
-		queue < pair<int, int>> curClould;
+        bool visited[51][51] = { false, };
+        queue<pair<int, int>> moved;
 
-		// 1. d 방향으로 s번 이동
-		for (int j = 0; j < size; j++)
-		{
-			int x = cloud.front().first;
-			int y = cloud.front().second;
+        int size = cloud.size();
+        while (size--) {
+            auto [x, y] = cloud.front();
+            cloud.pop();
 
-			for (int k = 0; k < s; k++)
-			{
-				x += dx[d];
-				y += dy[d];
+            int nx = ((x - 1 + dx[d] * s) % N + N) % N + 1;
+            int ny = ((y - 1 + dy[d] * s) % N + N) % N + 1;
 
-				if (x < 1) x = N;
-				if (x > N) x = 1;
-				if (y < 1) y = N;
-				if (y > N) y = 1;
+            A[nx][ny]++;
+            visited[nx][ny] = true;
+            moved.push({nx, ny});
+        }
 
-			}
+        // 물복사 버그
+        while (!moved.empty()) {
+            auto [x, y] = moved.front(); moved.pop();
 
-			// 2. 구름이 있는 곳 물 1 증가
-			map[x][y]++;
-			// 3. 구름 삭제
-			cloud.pop();
+            int cnt = 0;
+            for (int k = 2; k <= 8; k += 2) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
 
-			isVisited[x][y] = true;
-			curClould.push(make_pair(x,y));
-		}
+                if (nx < 1 || ny < 1 || nx > N || ny > N) continue;
+                if (A[nx][ny] > 0) cnt++;
+            }
+            A[x][y] += cnt;
+        }
 
+        // 새 구름 생성
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (!visited[i][j] && A[i][j] >= 2) {
+                    cloud.push({i, j});
+                    A[i][j] -= 2;
+                }
+            }
+        }
+    }
 
-		// 4. 물 복사
-		for (int j = 0; j < size; j++)
-		{
-			int x = curClould.front().first;
-			int y = curClould.front().second;
-			curClould.pop();
+    int ans = 0;
+    for (int i = 1; i <= N; i++)
+        for (int j = 1; j <= N; j++)
+            ans += A[i][j];
 
-			for (int k = 2; k < 9; k += 2)
-			{
-				int nx = x + dx[k];
-				int ny = y + dy[k];
-
-				if (nx < 1 && ny < 1 && nx >= N && nx >= N) continue;
-				if (map[nx][ny]==0) continue;
-
-				map[x][y]++;
-			}
-		}
-
-		//5. 구름 생성
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				if (map[i][j] < 2) continue;
-				if (isVisited[i][j]) continue;
-
-				cloud.push(make_pair(i, j));
-				map[i][j] -= 2;
-			}
-		}
-
-	}
-
-	int answer = 0;
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			answer += map[i][j];
-		}
-	}
-
-	cout << answer;
+    cout << ans;
 }
