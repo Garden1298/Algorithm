48e2b65e34f98c27b03ceabc041980eda407aae7
홍정원 <54656129+Garden1298@users.noreply.github.com>
2025-12-14 21:55:39 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,57 +1,44 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-
-int block[501][501];
 
 int main()
 {
-	int H, W;
-	cin >> H >> W;
+    int H, W;
+    cin >> H >> W;
 
-	int height;
-	for (int i = 0; i < W; i++)
-	{
-		cin >> height;
-		for (int j = 0; j < height; j++)
-		{
-			block[j][i] = 1;
-		}
-	}
+    int height[501];
+    for (int i = 0; i < W; i++)
+    {
+        cin >> height[i];
+    }
 
-	//위에서부터 아래로
-	for (int i = H - 1; i >= 0; i--)
-	{
-		int left = 0;
-		int right = W - 1;
+    int answer = 0;
 
-		//가장 왼쪽 블럭과 오른쪽 블럭이 있다면 해당하는 라인은 모두 물로 채우기 가능
-		if (block[i][left] == 1 && block[i][right] == 1) break;
+    // 양 끝은 물이 고일 수 없음
+    for (int i = 1; i < W - 1; i++)
+    {
+        int leftMax = 0;
+        int rightMax = 0;
 
-		while (block[i][left] == 0)
-		{
-			block[i][left] = -1;
-			left++;
-		}
+        // 왼쪽 최대 높이
+        for (int l = 0; l < i; l++)
+        {
+            leftMax = max(leftMax, height[l]);
+        }
 
-		while (block[i][right] == 0)
-		{
-			block[i][right] = -1;
-			right--;
-		}
-	}
+        // 오른쪽 최대 높이
+        for (int r = i + 1; r < W; r++)
+        {
+            rightMax = max(rightMax, height[r]);
+        }
 
-	int answer = 0;
+        int water = min(leftMax, rightMax) - height[i];
+        if (water > 0)
+        {
+            answer += water;
+        }
+    }
 
-	for (int i = 0; i < H; i++)
-	{
-		for (int j = 0; j < W; j++)
-		{
-			if (block[i][j] == 0)
-			{
-				answer++;
-			}
-		}
-	}
-
-	cout << answer << endl;
+    cout << answer << endl;
 }
