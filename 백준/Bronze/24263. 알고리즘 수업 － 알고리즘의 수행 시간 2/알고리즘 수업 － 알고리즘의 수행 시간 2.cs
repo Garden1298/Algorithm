79397acaa8be293ea470c99bf033dfefac86fdbf
홍정원 <54656129+Garden1298@users.noreply.github.com>
2026-01-03 13:20:44 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 48 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine(n);
+        Console.WriteLine(1);
+    }
+}
